1aa6ce12410e1ff3f94f98663ce5d8026d229ecb
Jacob <admin@auroraweb.com.au>
2024-05-07 21:43:53 +1000

    Refactor routes/web.php to include SubmissionsController resource

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
-use App\Http\Controllers\ProductsController;
 use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ListingController;
 use App\Http\Controllers\MainPageController;
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\SubmissionsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +19,7 @@ use App\Http\Controllers\MainPageController;
 |
 */
 
+Route::resource('/submissions', SubmissionsController::class);
 Route::resource('/products', ProductsController::class);
 
 Route::get('/hello', function () {
